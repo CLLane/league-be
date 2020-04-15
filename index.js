b58d530a0b777dffc09ec359d5078223635917d4
Chris Lane <pankokrumbs@gmail.com>
2020-04-15 14:00:08 -0600

    Refactor get endpoint

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 1234;
 
-app.listen(1234, () => {
-  console.log(`Server is listening on port: ${PORT}`);
-})
-
 app.get("/hello", (req, res) => {
   res.send("Hello World")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
 })
