3e6ec0c42355569c5b456ef4a53b67d92bc10c1c
Chris Lane <pankokrumbs@gmail.com>
2020-04-15 13:56:58 -0600

    First end point

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = 1234;
 
 app.listen(1234, () => {
-  console.log("Server is listening on port: 1234");
+  console.log(`Server is listening on port: ${PORT}`);
+})
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World")
 })
