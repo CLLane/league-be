eeb4bb502f49e329bf306236994d2551a291b651
Chris Lane <pankokrumbs@gmail.com>
2020-04-20 16:36:50 -0600

    Update key

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 1234;
 
-app.get("/hello", (req, res) => {
+app.get("/hello", async (req, res) => {
+  try {
+    const options = {
+      method: 'GET',
+      headers: {
+        'key': `${key}`,
+      }
+    };
+    const response = await fetch('http://ddragon.leagueoflegends.com/cdn/10.8.1/data/en_US/champion.json?RGAPI-57c4ffe6-681a-4399-9d19-7597445455e9', options)
+    if (!response.ok) {
+      throw new Error('Crap!')
+    }
+    const result = await response.json();
+    res.send(result.type)
+  } catch (error) {
+    throw new Error('Crap!!')
+  }
   res.send("Hello World")
 })
 
